4fd4983f896f2c4382ee4fc3a254600899ec0a24
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-13 18:24:45 +0100

    Add files via upload

---
+++ source/AboutsState.hx
@@ -0,0 +1,41 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+
+class AboutsState extends MainMenuState
+{
+    var BG:FlxSprite;
+    var Abouts:FlxText;
+    var Back:FlxButton; 
+    
+    override function create()
+    {
+
+        BG = new FlxSprite();
+		BG.loadGraphic("menuXxabouts.png");
+        BG.screenCenter();
+        add(BG);
+
+        Abouts = new FlxText(0, 0, 0, "that's mods fnf by Xxsans Team also thanks for download X3", 64);
+        Abouts.screenCenter();
+        Abouts.Color = 0xFF2F00FF;
+        add(Abouts);
+
+        Back = new FlxButton(0, 50, "Back", ClickBack);
+        Back.size = 32;
+        add(Back);
+        
+        super.create();
+    }
+
+    private function clickBack()
+    {
+        FlxG.camera.fade(FlxColor.BLACK, 0.33, false, () -> {
+            FlxG.sound.play(Paths.sound('cancelMenu'));
+            FlxG.switchState(new MainMenuState());
+        });
+    }
+}
